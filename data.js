ec14441db45788e91c9dc85c70b1995e08298d74
Federica Santoro <federica.santoro.1993@gmail.com>
2021-11-06 12:37:18 +0100

    data.js

---
+++ data.js
@@ -0,0 +1,22 @@
+const data = [
+    {
+        name: "Sage",
+        properties: "antimicrobial properties",
+        harvest: "June, July",
+        img:"https://cdn.w600.comps.canstockphoto.it/saggio-giardino-erba-vettore-eps_csp8458508.jpg"
+    },
+    {
+        name: "Rosemary",
+        properties: "digestive and antispasmodic properties",
+        harvest: "June",
+        img:"https://cdn.w600.comps.canstockphoto.it/erba-rosmarino-vettore-clipart_csp8458495.jpg"
+    },
+    {
+        name: "Lavender",
+        properties: "calming and antimicrobial properties",
+        harvest: "July",
+        img:"https://cdn.w600.comps.canstockphoto.it/saggio-giardino-erba-vettore-eps_csp8458508.jpg"
+    },
+    
+]
+    
